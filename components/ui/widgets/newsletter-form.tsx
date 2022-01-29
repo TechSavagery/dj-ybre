89a8d14bc91c92d7117b385a0e42d3e390e4dafd
ladellerby <ladellerby@gmail.com>
2022-01-29 01:29:28 -0800

    - added success message for add to mail list.

--- components/ui/widgets/newsletter-form.tsx
+++ components/ui/widgets/newsletter-form.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Fragment, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { CheckIcon } from '@heroicons/react/outline';
 
 export default function NewsletterForm() {
   const [formData, setFormData] = useState();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [hasSubmitted, setHasSubmitted] = useState(false);
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
+  const [open, setOpen] = useState(false);
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = async (data : any) => {
+  const onSubmit = async (data: any) => {
     setIsSubmitting(true);
     let response;
     setFormData(data);
@@ -22,12 +25,13 @@ export default function NewsletterForm() {
         body: JSON.stringify(data),
       });
       console.log(response);
+      if (response.ok == true) {
+        setOpen(true);
+      }
       setIsSubmitting(false);
-      setHasSubmitted(true); 
-      setEmail("")
-    } catch (err) {
-
-    }
+      setHasSubmitted(true);
+      setEmail('');
+    } catch (err) {}
   };
 
   function handleChange(e: any) {
@@ -43,109 +47,176 @@ export default function NewsletterForm() {
 
   return (
     <div className="relative mt-24 sm:mt-32 sm:py-16">
-    <div aria-hidden="true" className="hidden sm:block">
-      <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
-      <svg
-        className="absolute top-8 left-1/2 -ml-3"
-        width={404}
-        height={392}
-        fill="none"
-        viewBox="0 0 404 392"
-      >
-        <defs>
-          <pattern
-            id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
-            x={0}
-            y={0}
-            width={20}
-            height={20}
-            patternUnits="userSpaceOnUse"
-          >
-            <rect
-              x={0}
-              y={0}
-              width={4}
-              height={4}
-              className="text-gray-200"
-              fill="currentColor"
-            />
-          </pattern>
-        </defs>
-        <rect
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0 overflow-y-auto"
+          onClose={setOpen}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              leave="ease-in duration-200"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-90 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                <div>
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                    <CheckIcon
+                      className="h-6 w-6 text-green-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Subscribed
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Thank you for joining the list. Cant wait to send some &#128293; to your inbox! 
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-6">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-zinc-800 text-base font-medium text-white hover:bg-zinc-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-800 sm:text-sm"
+                    onClick={() => setOpen(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+      <div aria-hidden="true" className="hidden sm:block">
+        <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
+        <svg
+          className="absolute top-8 left-1/2 -ml-3"
           width={404}
           height={392}
-          fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)"
-        />
-      </svg>
-    </div>
-    <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="relative rounded-2xl px-6 py-10 bg-stone-500 overflow-hidden shadow-xl sm:px-12 sm:py-20">
-        <div
-          aria-hidden="true"
-          className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
+          fill="none"
+          viewBox="0 0 404 392"
         >
-          <svg
-            className="absolute inset-0 h-full w-full"
-            preserveAspectRatio="xMidYMid slice"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 1463 360"
-          >
-            <path
-              className="text-stone-400 text-opacity-40"
-              fill="currentColor"
-              d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
-            />
-            <path
-              className="text-stone-600 text-opacity-40"
-              fill="currentColor"
-              d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
-            />
-          </svg>
-        </div>
-        <div className="relative">
-          <div className="sm:text-center">
-            <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
-              Get notified when I&rsquo;m in the mix!
-            </h2>
-            <p className="mt-6 mx-auto max-w-2xl text-lg text-stone-100">
-              Whenever I have new content out I will send it to your
-              inbox. No promotions, just vibes I want to share with you!
-              Join my journey and tune in!
-            </p>
-          </div>
-          <form
-            action="#"
-            className="mt-12 sm:mx-auto sm:max-w-lg sm:flex"
-            id="newsletterform"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="min-w-0 flex-1">
-              <label htmlFor="cta-email" className="sr-only">
-                Email address
-              </label>
-              <input          
-                {...register('email', { required: true })}
-                onChange={handleChange}
-                placeholder="Enter your email"
-                id="email"
-                value={email}
-                type="email"
-                className="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-stone-500"
+          <defs>
+            <pattern
+              id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
               />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={392}
+            fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)"
+          />
+        </svg>
+      </div>
+      <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="relative rounded-2xl px-6 py-10 bg-stone-500 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
+          >
+            <svg
+              className="absolute inset-0 h-full w-full"
+              preserveAspectRatio="xMidYMid slice"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 1463 360"
+            >
+              <path
+                className="text-stone-400 text-opacity-40"
+                fill="currentColor"
+                d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
+              />
+              <path
+                className="text-stone-600 text-opacity-40"
+                fill="currentColor"
+                d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
+              />
+            </svg>
+          </div>
+          <div className="relative">
+            <div className="sm:text-center">
+              <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                Get notified when I&rsquo;m in the mix!
+              </h2>
+              <p className="mt-6 mx-auto max-w-2xl text-lg text-stone-100">
+                Whenever I have new content out I will send it to your inbox. No
+                promotions, just vibes I want to share with you! Join my journey
+                and tune in!
+              </p>
             </div>
-            <div className="mt-4 sm:mt-0 sm:ml-3">
-              <button
-                type="submit"
-                className="block w-full rounded-md border border-transparent px-5 py-3 bg-black text-base font-medium text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-stone-500 sm:px-10"
-              >
-                Notify me
-              </button>
-            </div>
-          </form>
+            <form
+              action="#"
+              className="mt-12 sm:mx-auto sm:max-w-lg sm:flex"
+              id="newsletterform"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="min-w-0 flex-1">
+                <label htmlFor="cta-email" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  {...register('email', { required: true })}
+                  onChange={handleChange}
+                  placeholder="Enter your email"
+                  id="email"
+                  value={email}
+                  type="email"
+                  className="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-stone-500"
+                />
+              </div>
+              <div className="mt-4 sm:mt-0 sm:ml-3">
+                <button
+                  type="submit"
+                  className="block w-full rounded-md border border-transparent px-5 py-3 bg-black text-base font-medium text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-stone-500 sm:px-10"
+                >
+                  Notify me
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
