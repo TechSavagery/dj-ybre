2cd8b89f053075f5b96bbaadf6e34e22a417be34
LaDell Erby <ladellerby@gmail.com>
2022-01-30 02:31:59 -0800

    - adjusting import of react-spotify-player.

--- pages/components/ui/widgets/spotify/spotify-card.tsx
+++ pages/components/ui/widgets/spotify/spotify-card.tsx
@@ -13,11 +13,11 @@ import {
   Heading,
 } from '@chakra-ui/react';
 import { motion, HTMLMotionProps } from 'framer-motion';
-import SpotifyPlayer from 'react-spotify-player';
 import React, { useState } from 'react';
 import { FaSpotify } from 'react-icons/fa';
 import { FiArrowRightCircle } from 'react-icons/fi';
 import ReactCardFlip from 'react-card-flip';
+const SpotifyPlayer = require('react-spotify-player')
 
 type Merge<P, T> = Omit<P, keyof T> & T;
 type MotionCardProps = Merge<HTMLChakraProps<'div'>, HTMLMotionProps<'div'>>;
