d33d282ee52d187b61a98b141e4212794a0a28c4
ladellerby <ladellerby@gmail.com>
2024-04-11 17:50:38 -0700

    add a / requests page

---
+++ src/app/requests/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { useEffect } from 'react'
+export default function Redirect() {
+  useEffect(() => {
+    window.location.assign('https://app.nosongrequests.com/djybre')
+  })
+  return <></>
+}
